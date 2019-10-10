ff55323ded903b9c6e31840253f283c0319d0e0f
katenkar23 <katenkar.ea@mail.ru>
2019-10-10 18:42:18 +0300

    Add Full Reference to CountMap

--- src/CountMap.java
+++ src/CountMap.java
@@ -1,14 +1,26 @@
 import java.util.Map;
 
 public interface CountMap<K, Integer> {
+
+    // добавляет элемент в этот контейнер
     void add(K key);
 
+    // возвращает количество добавлений данного элемента
+    int getCount(K key);
+
+    // удаляет элемент из контейнера и возвращает количество его добавлений (до удаления)
+    int remove(K key);
+
+    // количество разных элементов
+    int size();
+
+    // добавить все элементы из source в текущий контейнер, при совпадении ключей суммировать значения
     void addAll(CountMap<K, Integer> m2);
 
-    int count(K key);
+    // вернуть java.util.Map.ключ - добавленный элемент, значение - количество его добавлений
+    // <key, count>
+    Map toMap();
 
-    //<key, count>
-    Map asMap();
-
-    void copyTo(Map map); //<key, count>
+    // тот же самый контракт, как и toMap(), только всю информацию записать в destination
+    void toMap(Map destination); //<key, count>
 }
